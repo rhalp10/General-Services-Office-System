4248e3d2daa3c0ead0092937c652bcd4312ccac4
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2017-07-22 04:52:09 -0700

    accreceipt delete function

--- accreceipt_delete_action.php
+++ accreceipt_delete_action.php
@@ -1,4 +1,11 @@
-<?php 
-include('db.php');
+<?php
+    $ID = $_REQUEST['ID'];
+    include('db.php');
+    $sql ="DELETE FROM";
+    $sql.=" property_accountability_receipt_record WHERE ID = '$ID'";
+    $result = mysql_query($sql);
+    echo "<script>alert('Deleted successfully');
+                                        window.location='accreceipt.php';
+                                    </script>";
 
 ?>
